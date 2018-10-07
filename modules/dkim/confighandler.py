7a5519a056e8ae49c0ac6959ca9c813d4dfe8599
Stefan Helmert <stefan.helmert@informatik.tu-chemnitz.de>
2018-10-07 13:18:27 +0200

    defaulting dkim keyname to section name

--- modules/dkim/confighandler.py
+++ modules/dkim/confighandler.py
@@ -21,12 +21,14 @@ def applyDefault(config, defaultConfig={}):
 # DEFAULT section overwritten by handler default configuration overwr. by explicit configuration
 
 def interpreteConfig(cr, sh):
-    defaultDKIMConfig = {'keysize': 2048, 'keybasename': 'key'}
+    defaultDKIMConfig = {'keysize': 2048}
     dkimconfig = cr.getRawConfigOf('dkim')
     # apply general config defaults and the default section
     dkimconfig = applyDefault(dkimconfig, defaultDKIMConfig) # must be here because following section depends on default values
     for dkimSecName, content in dkimconfig.items():
         content = dict(content)
+        if not 'keybasename' in content:
+            content['keybasename'] = str(dkimSecName)
         if 'handler' in content:
             handlerNames = content['handler'].split('/')
             handler = __import__('modules.dkim.handler'+str(handlerNames[0]), fromlist=('modules','dkim'))
