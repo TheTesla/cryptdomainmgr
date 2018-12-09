b27204d4134cff68a66d093d1bf9007ce3f8cbf0
Stefan Helmert <stefan.helmert@informatik.tu-chemnitz.de>
2018-12-09 14:40:33 +0100

    Bugfix in confighandler defaulting priority of dhparam; ToDo: solve this in other confighandlers

--- modules/dhparam/confighandler.py
+++ modules/dhparam/confighandler.py
@@ -17,17 +17,22 @@ from simpleloggerplus import simpleloggerplus as log
 
 def interpreteConfig(cr, sh):
     defaultDHparamConfig = {'filename': '/etc/myssl/dhparam.pem', 'keysize': 2048}
-    dhparamconfig = cr.getRawConfigOf('dhparam')
+    dhparamconfigOrig = cr.getRawConfigOf('dhparam')
     # apply general config defaults and the default section
-    dhparamconfig = applyDefault(dhparamconfig, defaultDHparamConfig) # must be here because following section depends on default values
+    dhparamconfig = applyDefault(dhparamconfigOrig, defaultDHparamConfig) # must be here because following section depends on default values
 
+    log.debug(dhparamconfig)
     for dhparamSecName, content in dhparamconfig.items():
         content = dict(content)
         if 'handler' in content:
             log.debug('handler in content')
             handlerNames = content['handler'].split('/')
             handler = __import__('cryptdomainmgr.modules.dhparam.handler'+str(handlerNames[0]), fromlist=('cryptdomainmgr','modules','dhparam'))
+            log.debug(dhparamconfig)
             dhparamconfig[dhparamSecName].update(handler.defaultDHparamConfig)
+            log.debug(dhparamconfig)
+            dhparamconfig[dhparamSecName].update(dhparamconfigOrig[dhparamSecName])
+            log.debug(dhparamconfig)
         if 'keysize' in content:
             dhparamconfig[dhparamSecName]['keysize'] = int(content['keysize'])
     log.debug(dhparamconfig)
