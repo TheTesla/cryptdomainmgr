31167b4c52590185598e021afa41e854d6f01ce7
Stefan Helmert <stefan.helmert@t-online.de>
2020-04-14 01:21:42 +0200

    testing CLI

--- test/test_main.py
+++ test/test_main.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python
 # -*- encoding: UTF8 -*-
 
-
 #######################################################################
 #
 #    Copyright (c) 2020 Stefan Helmert <stefan@entroserv.de>
 #
 #######################################################################
 
-
 import unittest
 import subprocess as sp
-
-
 
 class TestMain(unittest.TestCase):
     def testMainHelp(self):
         stdout = sp.check_output("python3 -m cryptdomainmgr -h", shell=True)
         print(stdout[:5])
         self.assertEqual(b"usage", stdout[:5])
+
+    def testMainCDMupdate(self):
+        stdout = sp.check_output("python3 -m cryptdomainmgr --update \
+                                 --config-content \
+                                 '[cdm] statedir=/tmp/test_cryptdomaingmgr' \
+                                 2>&1", shell=True)
+        self.assertIn("update", str(stdout,"utf-8"))
+
+    def testMainCDMupdateFile(self):
+        stdout = sp.check_output("python3 -m cryptdomainmgr --update \
+                                 test/testconfigfile.conf \
+                                 2>&1", shell=True)
+        self.assertIn("update", str(stdout,"utf-8"))
 
 
 #    def testEmptyConfigContentList(self):
