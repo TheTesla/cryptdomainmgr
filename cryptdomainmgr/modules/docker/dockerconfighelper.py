566c30831b0019555aed100e668d9b699164bab3
Stefan Helmert <stefan.helmert@t-online.de>
2021-04-21 23:14:54 +0200

    dockerPathMap() argument order changed, default for dockersock: read from env; automatic detection, if running outside container

--- cryptdomainmgr/modules/docker/dockerconfighelper.py
+++ cryptdomainmgr/modules/docker/dockerconfighelper.py
@@ -13,10 +13,13 @@ def dockerMyContainerID():
             return ''
         return cgroup['1'][2]
 
-def dockerPathMap(dockersock, targetContainer, targetDirectory, sourceContainer=''):
+def dockerPathMap(targetContainer, targetDirectory, dockersock='', sourceContainer=''):
     if '' == sourceContainer:
         sourceContainer = dockerMyContainerID()
-    client = docker.DockerClient(base_url=dockersock)
+    if '' == dockersock:
+        client = docker.from_env()
+    else:
+        client = docker.DockerClient(base_url=dockersock)
     cAttrsbyName = {c.attrs['Name']: c.attrs for c in client.containers.list()}
     print(cAttrsbyName)
     cAttrsbyId = {c.id: c.attrs for c in client.containers.list()}
@@ -24,11 +27,14 @@ def dockerPathMap(dockersock, targetContainer, targetDirectory, sourceContainer=
         targetContAttrs = cAttrsbyName[targetContainer]
     elif targetContainer in cAttrsbyId.keys():
         targetContAttrs = cAttrsbyId[targetContainer]
-    if sourceContainer in cAttrsbyName.keys():
-        sourceContAttrs = cAttrsbyName[sourceContainer]
-    elif sourceContainer in cAttrsbyId.keys():
-        sourceContAttrs = cAttrsbyId[sourceContainer]
-    sourceMap = {e['Destination']: e['Source'] for e in sourceContAttrs['Mounts']}
+    if '' != sourceContainer:
+        if sourceContainer in cAttrsbyName.keys():
+            sourceContAttrs = cAttrsbyName[sourceContainer]
+        elif sourceContainer in cAttrsbyId.keys():
+            sourceContAttrs = cAttrsbyId[sourceContainer]
+        sourceMap = {e['Destination']: e['Source'] for e in sourceContAttrs['Mounts'] if 'rw' == e['Mode']}
+    else:
+        sourceMap = {'/': '/'}
     targetMap = {e['Destination']: e['Source'] for e in targetContAttrs['Mounts']}
     targetMounts = {os.path.normpath(d): os.path.normpath(s) for d, s in targetMap.items() if os.path.normpath(d) == os.path.commonpath([os.path.normpath(d),os.path.normpath(targetDirectory)])}
     targetSources = [os.path.join(s,os.path.relpath(os.path.normpath(targetDirectory), d)) for d, s in targetMounts.items()]
