66d13a9b5ba75eb0115e254ea3d38e40be674bbc
Stefan Helmert <stefan.helmert@t-online.de>
2021-05-22 01:33:23 +0200

    focal ist latest ubuntu release in travis -  install latest rspamd

--- install_rspamd.sh
+++ install_rspamd.sh
@@ -2,4 +2,6 @@
 
 wget -O- https://rspamd.com/apt-stable/gpg.key | sudo apt-key add -
 echo "deb http://rspamd.com/apt-stable/ $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/rspamd.list
+apt update
+apt install -y rspamd
 
