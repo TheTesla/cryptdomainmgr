014b7fd4c83d2e9fa8f70bf815f265da3074c2cc
Stefan Helmert <stefan.helmert@t-online.de>
2021-06-06 15:12:09 +0200

    handle git InvalidGitRepositoryError during pip install

--- cryptdomainmgr/__init__.py
+++ cryptdomainmgr/__init__.py
@@ -9,7 +9,8 @@ try:
     __version__ = '-'.join(repo.git.describe('--tags').split('-')[:2])
     with open('version', 'wt') as v:
         v.write(__version__)
-except ImportError as e:
+except Exception as e:
+#except ImportError as e:
     try:
         with open('version', 'rt') as v:
             __version__ = v.read()
