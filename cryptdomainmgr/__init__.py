95320bd1e588e4acd78e7d9811a1a541e8ca1292
Stefan Helmert <stefan.helmert@t-online.de>
2021-06-11 21:10:07 +0200

    __init__.py will only call git and update version if called directly

--- cryptdomainmgr/__init__.py
+++ cryptdomainmgr/__init__.py
@@ -4,11 +4,11 @@
 import os
 
 # VERSION
-__version__ = "0.2.4-3"
+__version__ = "0.2.4-4"
 
-try:
+
+if __name__ == '__main__':
     import git
-    #from git.exc import InvalidGitRepositoryError
     repo = git.Repo(search_parent_directories=False)
     version = '-'.join(repo.git.describe('--tags').split('-')[:2])
     if __version__ != version:
@@ -24,6 +24,4 @@ try:
             initfile = '\n'.join(lines)
         with open(os.path.realpath(__file__), 'wt') as v:
             v.write(initfile)
-#except ImportError as e:
-except Exception as e:
-    print(e)
+
